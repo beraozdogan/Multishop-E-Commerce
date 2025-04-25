cc07fa82b04bc9d011f7e48b6ef324963f3842f8
beraozdogan <61747031+beraozdogan@users.noreply.github.com>
2025-04-26 00:41:19 +0300

    mediator tasarım deseni eklendi handler command ve api işlemleri ile ilgili güncel geliştirmeler eklendi

--- Services/Discount/MultiShop.Discount/obj/Debug/net6.0/MultiShop.Discount.AssemblyInfo.cs
+++ Services/Discount/MultiShop.Discount/obj/Debug/net6.0/MultiShop.Discount.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Discount")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b1c0dede7eb075131b56da4ae25d2ae3dd491b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68552711885a6c64d6e9379f72b49371deabcd39")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Discount")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Discount")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
