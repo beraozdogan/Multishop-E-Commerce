1f472d4e2f5246a4fd57e5f95ac4898f05479cb1
beraozdogan <61747031+beraozdogan@users.noreply.github.com>
2025-05-12 04:20:28 +0300

    Basket mikrosservisi ve Redis entegrasyonu tamamlandı, frontend projesi eklendi

--- Services/Discount/MultiShop.Discount/obj/Debug/net6.0/MultiShop.Discount.AssemblyInfo.cs
+++ Services/Discount/MultiShop.Discount/obj/Debug/net6.0/MultiShop.Discount.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Discount")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a51e10fdea97630d731742987e7764eb846c380")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e054aca9aa84676339b770917edb39038cea5ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Discount")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Discount")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
