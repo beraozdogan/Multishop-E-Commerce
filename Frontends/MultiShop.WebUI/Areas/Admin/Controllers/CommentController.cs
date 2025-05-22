232ea3066ec423f4bf9302576b5c9917a2f8bc5b
beraozdogan <61747031+beraozdogan@users.noreply.github.com>
2025-05-22 03:45:49 +0300

    Yorum yönetimi geliştirildi: Admin paneli, veri setleri ve rating sistemi eklendi

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CommentController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CommentController.cs
@@ -42,7 +42,7 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
         public async Task<IActionResult> DeleteComment(string id)
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.DeleteAsync("https://localhost:7070/api/Comments?id=" + id);
+            var responseMessage = await client.DeleteAsync("https://localhost:7083/api/Comments?id=" + id);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index", "Comment", new { area = "Admin" });
@@ -59,7 +59,7 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
             ViewBag.v3 = "Yorum Listesi";
             ViewBag.v0 = "Yorum İşlemleri";
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7070/api/Comments/" + id);
+            var responseMessage = await client.GetAsync("https://localhost:7083/api/Comments/" + id);
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
@@ -72,11 +72,11 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
         [HttpPost]
         public async Task<IActionResult> UpdateComment(UpdateCommentDto updateCommentDto)
         {
-
+            updateCommentDto.Status = true;
             var client = _httpClientFactory.CreateClient();
             var jsonData = JsonConvert.SerializeObject(updateCommentDto);
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json"); ;
-            var responseMessage = await client.PutAsync("https://localhost:7070/api/Comments/", stringContent);
+            var responseMessage = await client.PutAsync("https://localhost:7083/api/Comments/", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index", "Comment", new { area = "Admin" });
--- Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
+++ Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
@@ -56,5 +56,12 @@ namespace MultiShop.Comment.Controllers
             _context.SaveChanges();
             return Ok("Yorum Başarıyla Güncellendi");
         }
+
+        [HttpGet("CommentListByProductId")]
+        public IActionResult CommentListByProductId(string id)
+        {
+            var values=_context.UserComments.Where(x=>x.ProductId==id).ToList();
+            return Ok(values);
+        }
     }
 }
