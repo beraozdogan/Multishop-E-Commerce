479488bed56cf99a054d1ce1f02c869e2faaa73a
beraozdogan <61747031+beraozdogan@users.noreply.github.com>
2025-05-24 01:06:45 +0300

    Kullanıcı girişi ve token deserialization işlemleri eklendi

---
+++ Frontends/MultiShop.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
+using MultiShop.WebUI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Text.Json;
+
+namespace MultiShop.WebUI.Controllers
+{
+	public class LoginController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public LoginController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index(CreateLoginDto createLoginDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var content = new StringContent(JsonSerializer.Serialize(createLoginDto), Encoding.UTF8, "application/json");
+			var response = await client.PostAsync("http://localhost:5101/api/Logins", content);
+			if (response.IsSuccessStatusCode)
+			{
+				var jsonData = await response.Content.ReadAsStringAsync();
+				var tokenModel = JsonSerializer.Deserialize<JwtResponseModel>(jsonData, new JsonSerializerOptions
+				{
+					PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+				});
+
+				if(tokenModel != null)
+				{
+					JwtSecurityTokenHandler handler=new JwtSecurityTokenHandler();
+					var token = handler.ReadJwtToken(tokenModel.Token);
+					var claims=token.Claims.ToList();
+
+
+				}
+			}
+		}
+	}
+}
