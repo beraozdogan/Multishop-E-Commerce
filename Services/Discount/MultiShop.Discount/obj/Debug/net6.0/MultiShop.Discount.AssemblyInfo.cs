ed1b7a7608577ca9721ef263c38e2bfd30f348a5
beraozdogan <61747031+beraozdogan@users.noreply.github.com>
2025-06-26 03:02:12 +0300

    Sipariş işlemleri için view component oluşturuldu, order API testleri tamamlandı, adres entity'sinde revizyonlar yapıldı, adres ekleme API metotları ve OrderAddress servisleri hazırlandı

--- Services/Discount/MultiShop.Discount/obj/Debug/net6.0/MultiShop.Discount.AssemblyInfo.cs
+++ Services/Discount/MultiShop.Discount/obj/Debug/net6.0/MultiShop.Discount.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Discount")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b87502c5b3f3d20ea1182a2011454e6b65884e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+151b3481444814cc31d7bbb1d04e7432f93f8a60")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Discount")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Discount")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
