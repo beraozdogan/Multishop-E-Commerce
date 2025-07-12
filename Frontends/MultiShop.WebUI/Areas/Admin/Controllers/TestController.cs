6c70da33c42b54017906b5c2a1bca579d6821f49
beraozdogan <61747031+beraozdogan@users.noreply.github.com>
2025-07-12 17:46:02 +0300

    Google Cloud Storage entegrasyonu tamamlandı, key dosyası gitignore'a alındı

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    public class TestController : Controller
+    {
+        [Area("Admin")]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
